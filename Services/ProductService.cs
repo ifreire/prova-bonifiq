e490b6c39c86a044a2241faf9f505ce07cb6704d
Iuri Freire <iuricostafreire@gmail.com>
2023-04-14 08:22:10 -0300

    Parte2.1

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using ProvaPub.Models;
 using ProvaPub.Repository;
+using System.Collections.Generic;
 
 namespace ProvaPub.Services
 {
@@ -14,8 +15,11 @@ namespace ProvaPub.Services
 
 		public ProductList  ListProducts(int page)
 		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
-		}
+			int totalCount = 10;
+			page = ((page > 0) ? page -= 1 : 0) * totalCount;
+
+            return new ProductList() { HasNext = false, TotalCount = totalCount, Products = _ctx.Products.Skip(page).Take(totalCount).ToList() };
+        }
 
 	}
 }
